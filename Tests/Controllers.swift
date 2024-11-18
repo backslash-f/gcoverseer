6ede0157a2ed1c2eb95d5c852ea097c21002842f
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-11-18 15:55:57 +0100

    Create Controllers Suite

---
+++ Tests/Controllers.swift
@@ -0,0 +1,14 @@
+import GameController
+import Testing
+@testable import GCOverseer
+
+@Suite struct Controllers {
+    @Test("Returns extended gamepads")
+    func extendedGamepadControllerTest() {
+        let extendedController = GCController.withExtendedGamepad()
+        let overseer = GCOverseer(controllers: [extendedController])
+        
+        #expect(overseer.extendedGamepadControllers().count == 1)
+        #expect(overseer.extendedGamepadControllers().contains(extendedController))
+    }
+}
