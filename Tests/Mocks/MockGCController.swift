bf4fc71c18af0cfe3ff0c25e9781f4d60644d3fe
Fernando Fernandes <fernando.fernandes@blacklane.com>
2024-12-04 17:28:55 +0100

    Update MockGCController
     - Add playerInded and motion support.

--- Tests/Mocks/MockGCController.swift
+++ Tests/Mocks/MockGCController.swift
@@ -2,13 +2,30 @@ import GameController
 
 class MockGCController: GCController {
     private let mockPhysicalInputProfile: GCPhysicalInputProfile
-
-    init(physicalInputProfile: GCPhysicalInputProfile) {
-        self.mockPhysicalInputProfile = physicalInputProfile
-        super.init()
-    }
+    private let mockMotion: GCMotion?
+    private var mockPlayerIndex: GCControllerPlayerIndex
 
     override var physicalInputProfile: GCPhysicalInputProfile {
         return mockPhysicalInputProfile
+    }
+
+    override var motion: GCMotion? {
+        return mockMotion
+    }
+
+    override var playerIndex: GCControllerPlayerIndex {
+        get { return mockPlayerIndex }
+        set { mockPlayerIndex = newValue }
+    }
+
+    init(
+        physicalInputProfile: GCPhysicalInputProfile,
+        motion: GCMotion? = nil,
+        playerIndex: GCControllerPlayerIndex = .indexUnset
+    ) {
+        self.mockPhysicalInputProfile = physicalInputProfile
+        self.mockMotion = motion
+        self.mockPlayerIndex = playerIndex
+        super.init()
     }
 }
